1981df3de57bebdb9740b60940eb69c265b0e6a5
Max <unitigrand@gmail.com>
2021-10-31 23:37:25 +0200

    add routes admin

--- client/routes.js
+++ client/routes.js
@@ -1,5 +1,10 @@
-export const authRoutes = [
+import Admin from './pages/Admin';
 
+export const authRoutes = [
+   {
+      path: '/admin',
+      Component: Admin 
+   } 
 ];
 
 export const publicRoutes = [
--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -4,7 +4,7 @@ import {Switch, Route, Redirect} from 'react-router-dom';
 const AppRouter = () => {
     return (
         <div>
-            
+            App
         </div>
     );
 };
