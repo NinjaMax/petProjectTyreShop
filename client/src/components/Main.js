78d22492891a7e10921381091d45384c182c2733
Max <unitigrand@gmail.com>
2021-11-07 23:56:43 +0200

    mdb main

---
+++ client/src/components/Main.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <header>
+          <Router>
+            <MDBNavbar color="indigo" dark expand="md" fixed="top">
+              <MDBNavbarBrand href="/">
+                <strong>Navbar</strong>
+              </MDBNavbarBrand>
+              {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
+              <MDBCollapse isOpen={this.state.collapse} navbar>
+                <MDBNavbarNav left>
+                  <MDBNavItem active>
+                    <MDBNavLink to="#">Home</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#">Link</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#">Profile</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBNavbar>
+          </Router>
+
+          <MDBView src="https://mdbootstrap.com/img/Photos/Others/img%20(50).jpg">
+            <MDBMask overlay="black-light" className="flex-center flex-column text-white text-center">
+              <h2>This Navbar is fixed</h2>
+              <h5>It will always stay visible on the top, even when you scroll down</h5>
+              <br />
+              <p>Full page intro with background image will be always displayed in full screen mode, regardless of device </p>
+            </MDBMask>
+          </MDBView>
+        </header>
+
+        <main>
+          <MDBContainer className="text-center my-5">
+            <p align="justify">Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </MDBContainer>
+        </main>
+        </div>
+    );
+};
+
+export default Main;
