b466df26d29c47f16ee0e95ebb84dc8221c643f4
NinjaMax <88530936+NinjaMax@users.noreply.github.com>
2022-06-28 09:12:54 +0300

    filtermain changed css

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -12,39 +12,37 @@ const NavBar = observer(() => {
       <a href="/#home">Home</a>
       <a href="/#news">News</a>
     <div className="dropdownNavbar">
-      <button className="dropbtnNavbar">Dropdown
-        <i className="fa fa-caret-down"></i>
-      </button>
-    <div className="dropdownContentNavbar">
-      <div className="headerNavbar">
-        <h2>Mega Menu</h2>
-      </div>
-      <div className="rowNavbar">
-        <div className="columnNavbar">
-          <h3>Category 1</h3>
-          <a href="/#">Link 1</a>
-          <a href="/#">Link 2</a>
-          <a href="/#">Link 3</a>
+        <button className="dropbtnNavbar">Dropdown
+          <i className="fa fa-caret-down"></i>
+        </button>
+      <div className="dropdownContentNavbar">
+        <div className="headerNavbar">
+          <h2>Mega Menu</h2>
         </div>
-        <div className="columnNavbar">
-          <h3>Category 2</h3>
-          <a href="/#">Link 1</a>
-          <a href="/#">Link 2</a>
-          <a href="/#">Link 3</a>
-        </div>
-        <div className="columnNavbar">
-          <h3>Category 3</h3>
-          <a href="/#">Link 1</a>
-          <a href="/#">Link 2</a>
-          <a href="/#">Link 3</a>
+        <div className="rowNavbar">
+          <div className="columnNavbar">
+            <h3>Category 1</h3>
+              <a href="/#">Link 1</a>
+              <a href="/#">Link 2</a>
+              <a href="/#">Link 3</a>
+          </div>
+          <div className="columnNavbar">
+            <h3>Category 2</h3>
+              <a href="/#">Link 1</a>
+              <a href="/#">Link 2</a>
+              <a href="/#">Link 3</a>
+          </div>
+          <div className="columnNavbar">
+            <h3>Category 3</h3>
+              <a href="/#">Link 1</a>
+              <a href="/#">Link 2</a>
+              <a href="/#">Link 3</a>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
-          
-        
-   
+
     );
 });
 export default NavBar;
