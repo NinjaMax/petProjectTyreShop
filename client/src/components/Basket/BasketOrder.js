a4d03aa52ec4adb9723ac98e2c2123e309a4669d
NinjaMax <88530936+NinjaMax@users.noreply.github.com>
2022-09-12 20:01:48 +0300

    cart create

--- client/src/components/Basket/BasketOrder.js
+++ client/src/components/Basket/BasketOrder.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import '../../css/BasketCss/BasketOrder.css';
+import TyresCardList from '../Cards/TyresCardList';
 
 const BasketOrder = () => {
     return (
-        <div>
+        <div className='basketOrder'>
             Оформлення замовлення
+            <div className='basketColmLeft'>
+                данні замовлення
+                <div>
+                    <input className='inputBasketOrderName' 
+                    type='text' 
+                    name='basketOrderName' 
+                    id='nameOrder' 
+                    placeholder="Прізвище ім'я Побатькові *" 
+                    required/>    
+                </div>
+                <div>
+                    <input className='inputBasketOrderPhone' 
+                    type='number' 
+                    name='basketOrderPhone' 
+                    id='phoneOrder' 
+                    placeholder="номер телефону *" 
+                    required/>    
+                </div>
+                <div>  
+                    <input className='inputBasketOrderEmail' 
+                    type='text' 
+                    name='basketOrderEmail' 
+                    id='emailOrder' 
+                    placeholder="email" 
+                    required/>    
+                </div>
+                <div>  
+                    <input className='inputBasketOrderAdress' 
+                    type='text' 
+                    name='basketOrderAdress' 
+                    id='adressOrder' 
+                    placeholder="місто" 
+                    required/>    
+                </div>
+                <span>Оплата</span>
+            </div>
+            <div className='basketColmRight'>
+                Товар і ціна 
+                остаточна сумма замовлення
+                <div>
+                    <TyresCardList/> 
+                      
+                </div>
+                <span>сумма</span>
+                <span>Всьго</span>
+                
+            </div>
         </div>
     );
 };
