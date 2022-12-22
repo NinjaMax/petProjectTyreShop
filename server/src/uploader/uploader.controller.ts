7bec6b2acc128499db5d916d5c5b74b68f40a9d4
NinjaMax <88530936+NinjaMax@users.noreply.github.com>
2022-12-22 20:42:35 +0200

    uploader parcer adjustment

--- server/src/uploader/uploader.controller.ts
+++ server/src/uploader/uploader.controller.ts
@@ -2,7 +2,8 @@ import { Controller, Get, Post, Body, Patch, Param, Delete,
   UploadedFile, UseInterceptors, ParseFilePipe, MaxFileSizeValidator,
    FileTypeValidator,
    ParseFilePipeBuilder,
-   HttpStatus, } from '@nestjs/common';
+   HttpStatus,
+   Header, } from '@nestjs/common';
 import { Express } from 'express';
 import { UploaderService } from './uploader.service';
 import { FileInterceptor } from '@nestjs/platform-express';
@@ -12,12 +13,14 @@ import { diskStorage } from 'multer';
 import { XMLParser } from 'fast-xml-parser';
 import { readFile } from 'fs';
 import * as fs from 'fs';
+import { DEFAULT_ENCODING } from 'crypto';
 
 @Controller('uploader')
 export class UploaderController {
   constructor(private readonly uploaderService: UploaderService) {}
 
   @Post()
+  @Header('Content-Transfer-Encoding', 'none')
   @UseInterceptors(FileInterceptor( 'file', {
     storage: diskStorage({
       destination: './downloads',
@@ -25,13 +28,16 @@ export class UploaderController {
 
         if(file.mimetype.indexOf('xml')) {
           file.mimetype = 'xml'
+          file.encoding = 'utf-8'
         }
+        
         const fileName: string = file.originalname;
         const newFileName: string = fileName;
         cb(null, `${newFileName}`)
-      }
+      }, 
+      
     })
-    } 
+  } 
   ))
   async uploadFileAndPassValidation(
     @Body() body: CreateUploaderDto, 
@@ -46,27 +52,61 @@ export class UploaderController {
     ) file: Express.Multer.File,
   )
   {
-    let xmlFile =  readFile('./downloads/' + file.originalname, 'utf-8',(err, data: Buffer) => {
+    //let xmlFile: string;
+    fs.readFile('./downloads/' + file.originalname, 'utf-8',(err, data: Buffer) => {
 
       if (err) throw err;
       //console.log("XMLFILE" + data.toString('utf-8'));
+      //const xmlFile: string = data.toString('utf-8', 3, data.length );
+      console.log(data);
+      //console.log(xmlFile);
+
+      const alwaysArray = [
+        "xml.items.item",
+      ];
+      
+      const options = {
+        ignoreAttributes : false,
+        ignorePiTags: true,
+        ignoreDeclaration: true,
+        alwaysCreateTextNode: false,
+        removeNSPrefix: true,
+        //preserveOrder: true,
+        processEntities: true,
+        format: true,
+        numberParseOptions: {
+          leadingZeros: true,
+          hex: true,
+          skipLike: /\+[0-9]{10}/
+        },
+        item:  "xml.items.item",
+        isArray: (item: string) => { 
+          if( alwaysArray.indexOf(item) !== -1) return true;
+        }
+      };
+    
+      const parser = new XMLParser(options);
+      let jsonObj = parser.parse(`${data}`);
+      let neWjsonObj = JSON.stringify(jsonObj, null);
+      
+      console.log(neWjsonObj);     
       //const newData: string = data.toString('utf-8');
 
-      //return newData;
+      //return xmlFile;
     })
 
-    console.log( xmlFile);
+    //console.log(xmlFile);
 
-    const options = {
-      ignoreAttributes : false
-    };
+    // const options = {
+    //   ignoreAttributes : false
+    // };
   
-    const parser = new XMLParser(options);
-    let jsonObj = parser.parse(`${xmlFile}`);
+    // const parser = new XMLParser(options);
+    // let jsonObj = parser.parse(`${xmlFile}`);
 
-    console.log( jsonObj);
+    //console.log( jsonObj);
 
-    return { body, file: file, jsonObj};
+    return { body, file: file,};
   }
 
   @Get()
