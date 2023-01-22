6d2e375387a73ce4fa8541eb8bd2f57b4d24accd
NinjaMax <88530936+NinjaMax@users.noreply.github.com>
2023-01-22 23:43:43 +0200

    set config entity to pm2

--- server/src/ecosystem.config.js
+++ server/src/ecosystem.config.js
@@ -1,33 +1,37 @@
 module.exports = {
   apps : [{
-    name: 'main',
-    script: './main.ts',
-    watch: true,
+    name: 'app',
+    script: 'ts-node',
+    args: './main.ts',
+    //watch: true,
     ignore_watch: ['node_modules', 'client'],
-    watch_delay: 1000,
-    node_args: ["--harmony", "dev --port=4000"],
-    interpreter: 'C:/Users/Work/AppData/Local/nodejs/node',
-    interpreter_args: "--harmony", 
+    //watch_delay: 1000,
+    //node_args: ["port=4000"],
+    interpreter: 'node',
+    //interpreter_args: "--harmony", 
     env: {
-      PORT: 'PORT',
+      PORT: '4000',
+      NODE_ENV: "development",
+      version: '0.0.1'
     },
     
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
   }, 
+  // {
+  //   script: './main.ts',
+  //   watch: ['./main.ts']
+  // }, 
   ],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  // deploy : {
+  //   production : {
+  //     user : 'SSH_USERNAME',
+  //     host : 'SSH_HOSTMACHINE',
+  //     ref  : 'origin/master',
+  //     repo : 'GIT_REPOSITORY',
+  //     path : 'DESTINATION_PATH',
+  //     'pre-deploy-local': '',
+  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+  //     'pre-setup': ''
+  //   }
+  // }
 };
